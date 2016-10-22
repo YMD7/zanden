bcb3f4a92b2eff3b99fd33eab5cb50690ca2193e
Kyo Yamada <hysteric.madam@gmail.com>
2016-10-22 21:31:13 +0900

    first commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,12 @@
+//= require jquery.min
+//= require_tree .
+
+// ----------------------------------------
+//                           + variables +
+// ----------------------------------------
+
+// -- + breakpoints + -------------
+var smartphone_screen_width     = 414;
+var tablet_screen_width         = 768;
+var medium_desktop_screen_width = 990;
+var large_desktop_screen_width  = 1200;
