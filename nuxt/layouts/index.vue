f73758f048e0db9cf950560a11a217b565e1d0b6
Kyo Yamada <hysteric.madam@gmail.com>
2020-01-12 20:23:55 +0900

    add: pages/index.vue

---
+++ nuxt/layouts/index.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+.container
+  nuxt
+</template>
+
+<script>
+</script>
+
+<style scoped lang="stylus">
+</style>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,70 +1,69 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        zanden
-      </h1>
-      <h2 class="subtitle" />
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+main
+  section
+    article.p-cover
+      a
+        h1
+          img.p-topics__image(src='~/assets/images/topics__image.png', alt='model 3000 | the new line amplifier')
+        .c-nav__link-arrow
+          img.c-icon__arrow-o(src='~/assets/images/arrow-o.svg')
+    ol.p-news
+      li
+        article.p-news__article
+          a
+            h2
+              img.p-topics__image(src='~/assets/images/topics__image.png', alt='Product Catalogue 2017 SS')
+          .c-nav__link-arrow
+            img.c-icon__arrow-o(src='~/assets/images/arrow-o.svg')
+    nav.c-nav__see-more
+      a
+        .c-nav__see-more-wrapper
+          span
+            | See more
+          img.c-icon__arrow(src='~/assets/images/arrow.svg')
+  nav.c-nav__menu
+    ul
+      li.c-nav__menu-item
+        a.c-nav__menu-anchor
+          span
+            | Products
+          img.c-icon__arrow(src='~/assets/images/arrow.svg')
+      li.c-nav__menu-item
+        a.c-nav__menu-anchor
+          span
+            | Our Brand
+          img.c-icon__arrow(src='~/assets/images/arrow.svg')
+      li.c-nav__menu-item
+        a.c-nav__menu-anchor
+          span
+            | Where to buy
+          img.c-icon__arrow(src='~/assets/images/arrow.svg')
+      li.c-nav__menu-item
+        a.c-nav__menu-anchor
+          span
+            | Contact
+          img.c-icon__arrow(src='~/assets/images/arrow.svg')
+  section
+    .c-sns__facebook
+      h4.c-sns__facebook-header
+        | Get our updates on Facebook.
+      .c-sns__facebook-wrapper
+        .c-sns__facebook-embed
+          .fb-page data-href="https://www.facebook.com/zandenaudio/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"
+            blockquote cite="https://www.facebook.com/zandenaudio/" class="fb-xfbml-parse-ignore"
+              a href="https://www.facebook.com/zandenaudio/"
+                | Zanden
+  nav.c-nav__back-to-top
+    a
+      img.c-icon__arrow-o--right(src='~/assets/images/arrow-o.svg')
+      span.c-nav__back-to-top-text
+        | BACK TO TOP
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
